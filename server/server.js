56426097adb121a41853ddd27a70805157d72375
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-12-30 21:59:14 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,9 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });   
 } 
-
+app.get("/",(req,res)=>{
+  res.send("good");
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
